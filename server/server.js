2279d6ac2643a1b14503d36f26ee1af214594d99
Riaz <mohd.riaz.2002@gmail.com>
2025-05-01 01:02:58 +0400

    integrated clerk authentication in websocket

--- server/server.js
+++ server/server.js
@@ -2,9 +2,15 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { YSocketIO } from "y-socket.io/dist/server";
+import { Clerk } from "@clerk/clerk-sdk-node";
+
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+
+const clerk = new Clerk({ secretKey: process.env.CLERK_SECRET_KEY });
 
 const io = new Server(server, {
   cors: {
@@ -13,12 +19,11 @@ const io = new Server(server, {
   },
 });
 
-io.of(/^\/yjs\|.*/).use((socket, next) => {
+io.of(/^\/yjs\|.*/).use(async (socket, next) => {
   const token = socket.handshake.auth?.token;
-  console.log("🪪 Token received from frontend:", token);
-  console.log("⚙️ Full handshake.auth:", socket.handshake.auth);
-  return next();
-  if (token === "valid-token") {
+  const authenticated = await authenticate(token);
+  console.log("authenticated: ", authenticated);
+  if (authenticated) {
     return next();
   }
   next(new Error("Unauthorized"));
@@ -32,3 +37,13 @@ const PORT = process.env.PORT || 1234;
 server.listen(PORT, () => {
   console.log(`✅ Yjs server running at http://localhost:${PORT}`);
 });
+
+async function authenticate(token) {
+  try {
+    // Verify the token using Clerk's SDK
+    const session = await clerk.verifyToken(token);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
